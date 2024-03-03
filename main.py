cb6c24ca9507d87085d2a20b9649242716d9f0b1
Jenya <boss59842009@gmail.com>
2024-03-03 17:01:03 +0200

    я все пофіксив, все працює, не міняй будь ласка знайчення fps, 40 кадрів достатньо, і від цієї цифри рахується кількість анімацій, не міняй!!!

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 from pygame import *
+
 '''Игровая сцена'''
 
 scr_W = 1280
 scr_H = 720
 
-fps = 60
+fps = 40
 
-screen = display.set_mode((scr_W, scr_H)) #Настраиваем размер окна
-display.set_caption('Shadows of the past') # называем нашу игру!
-display.set_icon(image.load('PH_Icon.png'))
+screen = display.set_mode((scr_W, scr_H))  # Настраиваем размер окна
+display.set_caption('Shadows of the past')  # называем нашу игру!
+display.set_icon(image.load('Placeholders/PH_Icon.png'))
 
-background = transform.scale(image.load('Placeholders/PH_Locations/PH_NightSky.jpg'), (scr_W, scr_H)) #добавление фона и настраиваем его
+background = transform.scale(image.load('Placeholders/PH_Locations/PH_NightSky.jpg'),
+                             (scr_W, scr_H))  # добавление фона и настраиваем его
 clock = time.Clock()
 
-#hero = transform.scale(image.load("Placeholders/PH_Anims/PH_RightAnim/PHR_1.png"),(90, 90)) # я с этим говном мучалась долго ибо ему всё не то
+# hero = transform.scale(image.load("Placeholders/PH_Anims/PH_RightAnim/PHR_1.png"),(90, 90)) # я с этим говном мучалась долго ибо ему всё не то
 # Крч, тут мы даем герою спарйт(1)
 '''Анимация персонажей'''
 
@@ -59,9 +61,11 @@ walk_down = [
 ]
 
 '''Шаблон пресонажей!'''
+
+
 class MainHero(sprite.Sprite):
     def __init__(self, player_img, x, y, w, h, speed):
-        sprite.Sprite.__init__(self) #говорим что наследуем sprite.Sprite
+        sprite.Sprite.__init__(self)  # говорим что наследуем sprite.Sprite
         self.w = w
         self.h = h
         self.image = transform.scale(image.load(player_img), (w, h))
@@ -73,7 +77,7 @@ class MainHero(sprite.Sprite):
         self.right = False
         self.up = False
         self.down = False
-        self.count = 0 #количество проиграных анимаций, изначально оно будет приравниватся 0
+        self.count = 0  # количество проиграных анимаций, изначально оно будет приравниватся 0
 
 
 class Player(MainHero):
@@ -87,28 +91,27 @@ class Player(MainHero):
             self.up = False
             self.down = False
 
-        if keys[K_a] and self.rect.x > 0:
+        elif keys[K_a] and self.rect.x > 0:
             self.rect.x -= self.speed
             self.left = True
             self.right = False
             self.up = False
             self.down = False
-            
-            
-        if keys[K_w] and self.rect.y > 0:
+
+        elif keys[K_w] and self.rect.y > 0:
             self.rect.y -= self.speed
             self.left = False
             self.right = False
             self.up = True
             self.down = False
-            
-        if keys[K_s] and self.rect.y < scr_H - self.h:
+
+        elif keys[K_s] and self.rect.y < scr_H - self.h:
             self.rect.y += self.speed
             self.left = False
             self.right = False
             self.up = False
             self.down = True
-            
+
         else:
             self.up = False
             self.down = False
@@ -117,7 +120,7 @@ class Player(MainHero):
             self.count = 0
 
     def animation(self):
-        if self.count + 1 >= fps:
+        if self.count >= fps:
             self.count = 0
         if self.left:
             screen.blit(transform.scale(walk_left[self.count // 10], (self.w, self.h)), (self.rect.x, self.rect.y))
@@ -133,26 +136,20 @@ class Player(MainHero):
         elif self.down:
             screen.blit(transform.scale(walk_down[self.count // 10], (self.w, self.h)), (self.rect.x, self.rect.y))
             self.count += 1
-
         else:
             screen.blit(self.image, (self.rect.x, self.rect.y))
             self.count = 0
 
-    
 
 '''Персонажи'''
 
-#hero = Player('images/hero/right/PHR_1.png', 100, 300, 90, 90, 8)
+# hero = Player('images/hero/right/PHR_1.png', 100, 300, 90, 90, 8)
 hero = Player('Placeholders/PH_Left.png', 100, 30, 90, 90, 5)
 
 '''Игровой цикл'''
 game = True
 
-x = 100
-y = 300
-speed = 10
-
-while game: # Цикл игры
+while game:  # Цикл игры
     screen.blit(background, (0, 0))
     for e in event.get():
         if e.type == QUIT:
